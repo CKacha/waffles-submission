25ea49e45c3a1c4555c8bd9ed5670ccad56d75db
Ckacha <thisischanmin@gmail.com>
2026-06-18 00:58:33 -0400

    added setInterval

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # waffles
-Waffles submission!
+My [Waffles](http://waffles.hackclub.com/) submission!
 
-It's a smol litle website about some snaks :p
+It's a smol litle website about smoothies vs icecream!
+
+Hiya Nova :3
--- script.js
+++ script.js
@@ -26,6 +26,42 @@ function setupBlendChecker() {
   });
 }
 
+function setupImageRotation() {
+  const icecreamImg = document.getElementById("iceCreamImg");
+  const smoothieImg = document.getElementById("smoothieImg");
+
+  if (icecreamImg) {
+    const icecreamImages = [
+      "images/icecream1.jpg",
+      "images/icecream2.jpg",
+      "images/icecream3.jpg"
+    ];
+
+    let currentIcecreamIndex = 0;
+
+    setInterval(() => {
+      currentIcecreamIndex = (currentIcecreamIndex + 1) % icecreamImages.length;
+      icecreamImg.src = icecreamImages[currentIcecreamIndex];
+    }, 5000);
+  }
+
+  if (smoothieImg) {
+    const smoothieImages = [
+      "images/smoothie1.jpg",
+      "images/smoothie2.jpg",
+      "images/smoothie3.jpg"
+    ];
+
+    let currentSmoothieIndex = 0;
+
+    setInterval(() => {
+      currentSmoothieIndex = (currentSmoothieIndex + 1) % smoothieImages.length;
+      smoothieImg.src = smoothieImages[currentSmoothieIndex];
+    }, 5000);
+  }
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   setupBlendChecker();
+  setupImageRotation();
 });
